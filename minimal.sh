903a021fe8a4401255b4982926b3d2a0713a75c9
Onat <73953584+du0ks@users.noreply.github.com>
2023-03-08 22:38:39 +0300

    Update minimal.sh

--- minimal.sh
+++ minimal.sh
@@ -1,5 +1,5 @@
 sudo dnf install @base-x -y #Basic system components and drivers.
-sudo dnf install @development-tools -y #Some tools like wget.
+sudo dnf install @development-tools -y #Some tools like git.
 sudo dnf install gnome-shell gnome-terminal gnome-tweaks gnome-disk-utility gnome-extensions gnome-text-editor gnome-browser-connector nano htop flatpak inxi nautilus git flatpak ffmpeg-libs powertop -y
 sudo systemctl enable gdm
 sudo systemctl set-default graphical.target
